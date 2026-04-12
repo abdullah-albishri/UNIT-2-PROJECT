b7d831dea8d645f070e0b4f48b14661bddeb2f20
abdullah <a.m.albshri22@gmail.com>
2026-04-12 23:36:49 +0300

    alomst done

--- Saudi_League/main/urls.py
+++ Saudi_League/main/urls.py
@@ -6,5 +6,12 @@ app_name = "main"
 
 urlpatterns = [
     path('', views.home_view, name="home"),
+    path('football_male/', views.football_male_view, name="football_male"),
+    path('football_female/', views.football_female_view, name="football_female"),
+    path('stadiums/', views.stadiums_view, name="stadiums"),
+    path('moments/', views.moments_view, name="moments"),
+    path('worldcup2034/', views.worldcup2034_view, name="worldcup2034"),
+    path('contact_us/', views.contact_us_view, name="contact_us"),
+    path('mode/<mode>/',views.mode_view, name='mode_view'),
 
 ]
--- Saudi_League/main/views.py
+++ Saudi_League/main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
 
 def base_view(request: HttpRequest):
@@ -6,3 +6,30 @@ def base_view(request: HttpRequest):
 
 def home_view(request: HttpRequest) :
     return render(request, "main/home.html")
+
+def football_male_view(request: HttpRequest) :
+    return render(request, "main/football_male.html")
+
+def football_female_view(request: HttpRequest) :
+    return render(request, "main/football_female.html")
+
+def stadiums_view(request: HttpRequest) :
+    return render(request, "main/stadiums.html")
+
+def worldcup2034_view(request: HttpRequest):
+    return render(request, "main/worldcup2034.html")
+
+def moments_view(request: HttpRequest):
+    return render(request, "main/unforgettable_moments.html")
+
+def contact_us_view(request: HttpRequest) :
+    return render(request, "main/contact_us.html")
+
+def mode_view(request: HttpRequest, mode) :
+    response = redirect(request.GET.get("next", "/"))
+    if mode == 'dark' :
+        response.set_cookie('mode', 'dark', max_age=24*60*60, path='/', samesite='Lax')
+    elif mode == 'light' :
+        response.set_cookie('mode', 'light', max_age=24*60*60, path='/', samesite='Lax')
+
+    return response
